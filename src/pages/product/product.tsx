2c95802067566c940dcd64ccd850125890484db6
Babarhusyn <babarhusain262@gmail.com>
2024-09-17 23:31:08 +0500

    second

---
+++ src/pages/product/product.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import foodData from '../../../dataApi.json'
+
+
+
+
+const product = () => {
+  return (
+    <div className='product-grid'>
+        <div className='product-main'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default product
